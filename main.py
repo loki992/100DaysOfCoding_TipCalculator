751dac0052f3e3ef1ae1a104427c63446c8bb34c
loki992 <32733969+loki992@users.noreply.github.com>
2024-07-15 20:39:02 +0000

    initial commit - basic tip calc

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Welcome to the Band Name Generator.")
-city_name = input("What's the name of the city you grew in?\n")
-pet_name = input("What's your pet's name?\n")
-print("Your band name could be " + city_name + " " + pet_name)
+print("Welcome to the tip calculator!")
+total_bill = round(int(input("What was the total bill? $ ")), 2)
+tip_percentage = float(input("How much tip would you like to give? 10%, 12% or 15%? "))
+people_quant = int(input("How many people to split the bill? "))
+total_with_tip = total_bill/(100/tip_percentage)+total_bill
+payment_per_person = round(total_with_tip/people_quant, 2)
+print(f"Each person should pay: ${payment_per_person}")
