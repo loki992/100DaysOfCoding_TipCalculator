773c3c5670ef0cc8915989963ff737b7430738c0
loki992 <32733969+loki992@users.noreply.github.com>
2024-07-15 20:53:59 +0000

    small cleaning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Welcome to the tip calculator!")
 total_bill = round(int(input("What was the total bill? $ ")), 2)
-tip_percentage = float(input("How much tip would you like to give? 10%, 12% or 15%? "))
+tip_percentage = float(input("How much % tip would you like to give? 10, 12 or 15? "))
 people_quant = int(input("How many people to split the bill? "))
 total_with_tip = total_bill/(100/tip_percentage)+total_bill
 payment_per_person = round(total_with_tip/people_quant, 2)
